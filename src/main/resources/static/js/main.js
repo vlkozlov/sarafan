cc553596b1e831bcaefa8bd6df7f3d54d3ab4182
vkozlov <vl-kozlov2009@yandex.ru>
2019-10-08 00:26:15 +0300

    feature: update interface

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,27 @@
+function getIndex (list, id) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id === id) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 var messageApi = Vue.resource('/message{/id}');
 
 Vue.component("message-form", {
-    props: ['messages'],
+    props: ['messages', 'messageAttr'],
     data: function() {
         return {
-            text: ''
+            text: '',
+            id: ''
+        }
+    },
+    watch: {
+        messageAttr: function(newVal, oldVal) {
+            this.text = newVal.text;
+            this.id = newVal.id;
         }
     },
     template:
@@ -16,27 +33,62 @@ Vue.component("message-form", {
         save:function() {
             var message = {text: this.text};
 
-            messageApi.save({}, message).then(result =>
+            if (this.id) {
+                messageApi.update({id: this.id}, message).then(result =>
                     result.json().then(data => {
-                    this.messages.push(data);
-                    this.text ='';
-                })
-            )
+                        var index = getIndex(this.messages, data.id);
+                        this.messages.splice(index, 1, data);
+                this.id = '';
+                this.text = '';
+                    })
+                )
+            } else {
+                messageApi.save({}, message).then(result =>
+                    result.json().then(data => {
+                        this.messages.push(data);
+                        this.text = '';
+                    })
+                )
+            }
         }
     }
 
 });
 
 Vue.component('message-row', {
-    props: ['message'],
-    template: '<div><i>({{ message.id }})</i>{{ message.text }}</div>'
+    props: ['message', 'editMethod', 'messages'],
+    template: '<div style="position: absolute; right: 0">' +
+        '<i>({{ message.id }})</i>{{ message.text }}' +
+        '<span>' +
+            '<input type="button" value="Edit" @click="edit" />' +
+            '<input type="button" value="X" @click="del" />' +
+        '</span>' +
+        '</div>',
+    methods: {
+        edit: function () {
+            this.editMethod(this.message);
+        },
+        del: function () {
+            messageApi.remove({id: this.message.id}).then(result =>
+                if (result.ok) {
+                    this.messages.splice(this.messages.indexOf(this.message), 1)
+                }
+            );
+        }
+    }
 });
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div>' +
-        '<message-form :messages="messages" />' +
-        '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+    data: function() {
+        return {
+            message: null
+        }
+    },
+    template: '<div style="position: relative; width: 380px;">' +
+        '<message-form :messages="messages" :messageAttr="message" />' +
+        '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
+        ':editMethod="editMethod" :messages="messages" />' +
         '</div>',
     created: function() {
         messageApi.get().then(result =>
@@ -44,6 +96,11 @@ Vue.component('messages-list', {
                 data.forEach(message => this.messages.push(message))
             )
         )
+    },
+    methods: {
+        editMethod: function (message) {
+            this.message = message;
+        }
     }
 });
 
